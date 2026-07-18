127703d25b744f6c29ca81669905cc44caef1cf1
among_earth <sjm4104@gmail.com>
2026-07-19 07:35:08 +0900

    Add files via upload

--- math-render.js
+++ math-render.js
@@ -48,7 +48,26 @@
       [/\\lambda_\{\\text\{max\}\} = \\frac\{2\.898 \\times 10\^6\}\{T\}/g,
         '$$\\lambda_{\\max} = \\frac{2.898 \\times 10^6}{T}$$'],
       [/M = 4\.83 - 2\.5 \\log_\{10\}\(L \/ L_\{\\odot\}\)/g,
-        '$$M = 4.83 - 2.5 \\log_{10}\\left(\\frac{L}{L_\\odot}\\right)$$']
+        '$$M = 4.83 - 2.5 \\log_{10}\\left(\\frac{L}{L_\\odot}\\right)$$'],
+      [/GMm\/\(r\^2\)/g, '$$F = \\frac{GMm}{r^2}$$'],
+      [/\$\$F = mv\^2\/r = mrw\^2\$\$/g, '$$F = \\frac{mv^2}{r} = mr\\omega^2$$'],
+      [/\$\$\(m1\+m2\) Xcm = m1x1 \+ m2x2\$\$/g,
+        '$$(m_1+m_2)X_{\\mathrm{cm}}=m_1x_1+m_2x_2$$'],
+      [/\$\$\(L\/L\(\ud0dc\uc591\)\) = \(M \/ M\(\ud0dc\uc591\)\)\^a\$\$/g,
+        '$$\\frac{L}{L_\\odot}=\\left(\\frac{M}{M_\\odot}\\right)^a$$'],
+      [/\$\$T = T\(\ud0dc\uc591\)\u00b7sqrt\(M \/ M\(\ud0dc\uc591\)\)\$\$/g,
+        '$$T=T_\\odot\\sqrt{\\frac{M}{M_\\odot}}$$'],
+      [/\$\$r = a\(1 - e\u00b2\) \/ \(1 \+ e\u00b7cos \u03b8\)\$\$/g,
+        '$$r=\\frac{a(1-e^2)}{1+e\\cos\\theta}$$'],
+      [/\$\$E = K \+ U = \(1\/2\)mv\u00b2 - GMm\/r = -GMm \/ \(2a\)\$\$/g,
+        '$$E=K+U=\\frac{1}{2}mv^2-\\frac{GMm}{r}=-\\frac{GMm}{2a}$$'],
+      [/\$\$v\u00b2 = GM \u00b7 \(2\/r - 1\/a\)\$\$/g,
+        '$$v^2=GM\\left(\\frac{2}{r}-\\frac{1}{a}\\right)$$'],
+      [/x = a\(cos E - e\)  y = bsinE/g, '$$x=a(\\cos E-e),\\quad y=b\\sin E$$'],
+      [/2c = 2ae/g, '$$2c=2ae$$'],
+      [/F2 = \(-2ae, 0\)/g, '$$F_2=(-2ae,0)$$'],
+      [/A = \u03c0ab x delt\/T/g, '$$A=\\frac{\\pi ab\\,\\Delta t}{T}$$'],
+      [/T\^2 = a\^3/g, '$$T^2=a^3$$']
     ];
     const normalized = fixes.reduce((html, [pattern, replacement]) => html.replace(pattern, replacement), original);
     if (normalized !== original) root.innerHTML = normalized;
